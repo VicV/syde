35682658e311af57322c45617f23f11a3366e1f7
Adam Wootton <ajwootto@uwaterloo.ca>
2015-10-23 17:32:58 -0400

    stuff

---
+++ Android/app/src/main/java/com/jarone/litterary/DJIBaseActivity.java
@@ -0,0 +1,27 @@
+package com.jarone.litterary;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import dji.midware.data.manager.P3.ServiceManager;
+
+public class DJIBaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djiconnection_manager);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ServiceManager.getInstance().pauseService(false); // Resume the service
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ServiceManager.getInstance().pauseService(true); // Pause the service
+    }
+}
