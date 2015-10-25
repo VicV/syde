1c7672ec19f29a42856c4782705a0a28678a94a8
vicv <victorvuc@gmail.com>
2015-10-24 22:32:45 -0400

    lalalala

---
+++ Android/app/src/main/java/com/jarone/litterary/activities/DJIAoaActivity.java
@@ -0,0 +1,51 @@
+package com.jarone.litterary.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+
+import com.jarone.litterary.R;
+
+import dji.midware.data.manager.P3.ServiceManager;
+import dji.midware.usb.P3.DJIUsbAccessoryReceiver;
+import dji.midware.usb.P3.UsbAccessoryService;
+
+public class DJIAoaActivity extends Activity {
+
+    private static boolean isStarted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_djiaoa);
+        connectAoa();
+    }
+
+    //TODO: Just put in main onResume
+    private void connectAoa() {
+
+        if (!isStarted) {
+            isStarted = true;
+            ServiceManager.getInstance();
+            UsbAccessoryService.registerAoaReceiver(this);
+            Intent intent = new Intent(DJIAoaActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            //Do nothing
+        }
+
+
+
+        Intent aoaIntent = getIntent();
+        if (aoaIntent != null) {
+            String action = aoaIntent.getAction();
+            if (action.equals(UsbManager.ACTION_USB_ACCESSORY_ATTACHED) || action.equals(Intent.ACTION_MAIN)) {
+                Intent attachedIntent = new Intent();
+                attachedIntent.setAction(DJIUsbAccessoryReceiver.ACTION_USB_ACCESSORY_ATTACHED);
+                sendBroadcast(attachedIntent);
+            }
+        }
+        finish();
+    }
+}
