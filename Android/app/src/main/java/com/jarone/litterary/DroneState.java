7403464741af0bbdf28a55697bc21c28a56e89ec
Adam Wootton <ajwootto@uwaterloo.ca>
2015-10-24 22:13:09 -0400

    wip

--- Android/app/src/main/java/com/jarone/litterary/DroneState.java
+++ Android/app/src/main/java/com/jarone/litterary/DroneState.java
@@ -12,20 +12,32 @@ import dji.sdk.interfaces.DJIMcuUpdateStateCallBack;
 public class DroneState {
     // Update the drone location based on states from MCU.
 
-    private double latitude = 0;
-    private double longitude = 0;
+    private static double latitude = 0;
+    private static double longitude = 0;
 
-    private double homeLatitude;
-    private double homeLongitude;
+    private static double speed = 0;
+    private static double altitude = 0;
 
-    private boolean connected = false;
-    public boolean hasTask = false;
+    private static double pitch = 0;
+    private static double roll = 0;
+    private static double yaw = 0;
+
+    private static double velocityX = 0;
+    private static double velocityY = 0;
+    private static double velocityZ = 0;
+
+
+    private static double homeLatitude;
+    private static double homeLongitude;
+
+    private static boolean connected = false;
+    public static boolean hasTask = false;
 
     private static final String TAG = DroneState.class.toString();
 
-    private DJIMcuUpdateStateCallBack mMcuUpdateStateCallBack;
+    private static DJIMcuUpdateStateCallBack mMcuUpdateStateCallBack;
 
-    public void updateDroneLocation(){
+    public static void updateDroneLocation(){
         mMcuUpdateStateCallBack = new DJIMcuUpdateStateCallBack(){
 
             @Override
@@ -34,16 +46,26 @@ public class DroneState {
                 longitude = state.droneLocationLongitude;
                 homeLatitude = state.homeLocationLatitude;
                 homeLongitude = state.homeLocationLongitude;
+
+                altitude = state.altitude;
+                speed = state.speed;
+
+                velocityX = state.velocityX;
+                velocityY = state.velocityY;
+                velocityZ = state.velocityZ;
+                pitch = state.pitch;
+                roll = state.roll;
+                yaw = state.yaw;
             }
         };
         Log.e(TAG,"setMcuUpdateState");
         DJIDrone.getDjiMC().setMcuUpdateStateCallBack(mMcuUpdateStateCallBack);
     }
 
-    public double getLatitude() {
+    public static double getLatitude() {
         return latitude;
     }
-    public double getLongitude() {
+    public static double getLongitude() {
         return longitude;
     }
 }
