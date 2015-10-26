8030b931b7383aadba7139cd478e2e0ea82c7d22
Adam Wootton <ajwootto@uwaterloo.ca>
2015-10-25 21:59:01 -0400

    add view to bitmap code for image processing

--- Android/app/src/main/java/com/jarone/litterary/VisionProcessor.java
+++ Android/app/src/main/java/com/jarone/litterary/VisionProcessor.java
@@ -1,11 +1,13 @@
 package com.jarone.litterary;
 
+import android.graphics.Bitmap;
+
 /**
  * Created by Adam on 2015-10-17.
  */
 public class VisionProcessor {
 
-    public void processFrame(byte[] videoBuffer, int size) {
+    public void processFrame(Bitmap frame) {
     }
 
 }
