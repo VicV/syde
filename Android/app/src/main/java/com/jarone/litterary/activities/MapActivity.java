f7a6bfe667bc3fd40db5deb3cea8230dac2274c1
vicv <victorvuc@gmail.com>
2015-11-09 15:17:33 -0500

    Added new location key

---
+++ Android/app/src/main/java/com/jarone/litterary/activities/MapActivity.java
@@ -0,0 +1,15 @@
+package com.jarone.litterary.activities;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by vic on 11/9/15.
+ */
+public class MapActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
