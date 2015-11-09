fd7c37878ccac01d8f093cbfa8d2207349fb43ee
vicv <victorvuc@gmail.com>
2015-11-09 14:58:47 -0500

    click listeners

--- Android/app/src/main/java/com/jarone/litterary/activities/MainActivity.java
+++ Android/app/src/main/java/com/jarone/litterary/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.jarone.litterary.activities;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.View;
 
 import com.jarone.litterary.GroundStation;
 import com.jarone.litterary.R;
@@ -17,12 +18,16 @@ public class MainActivity extends DJIBaseActivity {
     private DjiGLSurfaceView mDjiGLSurfaceView;
 
     private static final String TAG = MainActivity.class.toString();
+
     //Activity is starting.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setOnClickListeners();
+
         registerCamera();
+
         GroundStation.withConnection(new Runnable() {
             @Override
             public void run() {
@@ -31,33 +36,55 @@ public class MainActivity extends DJIBaseActivity {
         });
     }
 
-    public void testPlan() {
-        GroundStation.newTask();
-        GroundStation.defaultAltitude = 20;
-        GroundStation.defaultSpeed = 1;
-        GroundStation.addPoint(10, 10);
-        GroundStation.addPoint(10, 10);
-        GroundStation.addPoint(10, 10);
-        GroundStation.addPoint(10, 10);
 
-        GroundStation.withConnection(new Runnable() {
+    public View.OnClickListener getDirectionalListener() {
+        return new View.OnClickListener() {
             @Override
-            public void run() {
-                GroundStation.uploadAndExecuteTask();
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button_up:
+                        //TODO: Up Code
+                        break;
+                    case R.id.button_down:
+                        //TODO: Down Code
+                        break;
+                    case R.id.button_left:
+                        //TODO: Left Code
+                        break;
+                    case R.id.button_right:
+                        //TODO: Right Code
+                        break;
+                }
             }
-        });
+        };
+    }
+
+    public View.OnClickListener getHomeButtonListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button_go_home:
+                        //TODO: Go Home Code
+                        break;
+                    case R.id.button_set_home:
+                        //TODO: Set Home Code
+                        break;
+                }
+            }
+        };
+    }
+
+    public View.OnClickListener setAltitudeListener() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Set Altitude Code
+            }
+        };
     }
 
 
-    private Bitmap viewToBitmap(DjiGLSurfaceView view) {
-
-
-            Bitmap b = Bitmap.createBitmap(view.getLayoutParams().width, view.getLayoutParams().height, Bitmap.Config.ARGB_8888);
-            Canvas c = new Canvas(b);
-            view.layout(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
-            view.draw(c);
-            return b;
-        }
     private void registerCamera() {
         mDjiGLSurfaceView = (DjiGLSurfaceView) findViewById(R.id.DjiSurfaceView_02);
         mDjiGLSurfaceView.start();
@@ -72,12 +99,44 @@ public class MainActivity extends DJIBaseActivity {
         DJIDrone.getDjiCamera().setReceivedVideoDataCallBack(mReceivedVideoDataCallBack);
     }
 
-    @Override
-    protected void onDestroy() {
-        if (DJIDrone.getDjiCamera() != null) {
-            DJIDrone.getDjiCamera().setReceivedVideoDataCallBack(null);
-        }
-        super.onDestroy();
+
+    private void setOnClickListeners() {
+        findViewById(R.id.button_down).setOnClickListener(getDirectionalListener());
+        findViewById(R.id.button_left).setOnClickListener(getDirectionalListener());
+        findViewById(R.id.button_right).setOnClickListener(getDirectionalListener());
+        findViewById(R.id.button_up).setOnClickListener(getDirectionalListener());
+        findViewById(R.id.button_go_home).setOnClickListener(getHomeButtonListener());
+        findViewById(R.id.button_set_home).setOnClickListener(getHomeButtonListener());
+        findViewById(R.id.button_set_altitude).setOnClickListener(getHomeButtonListener());
+
     }
+
+
+    private Bitmap viewToBitmap(DjiGLSurfaceView view) {
+        Bitmap b = Bitmap.createBitmap(view.getLayoutParams().width, view.getLayoutParams().height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(b);
+        view.layout(view.getLeft(), view.getTop(), view.getRight(), view.getBottom());
+        view.draw(c);
+        return b;
+    }
+
+
+//    public void testPlan() {
+//        GroundStation.newTask();
+//        GroundStation.defaultAltitude = 20;
+//        GroundStation.defaultSpeed = 1;
+//        GroundStation.addPoint(10, 10);
+//        GroundStation.addPoint(10, 10);
+//        GroundStation.addPoint(10, 10);
+//        GroundStation.addPoint(10, 10);
+//
+//        GroundStation.withConnection(new Runnable() {
+//            @Override
+//            public void run() {
+//                GroundStation.uploadAndExecuteTask();
+//            }
+//        });
+//    }
+
 
 }
