1d6427ebe652efb772d564197f5befd498f84ecf
Adam Wootton <ajwootto@uwaterloo.ca>
2015-11-09 14:27:22 -0500

    toasted error notifications

--- Android/app/src/main/java/com/jarone/litterary/handlers/ErrorHandler.java
+++ Android/app/src/main/java/com/jarone/litterary/handlers/ErrorHandler.java
@@ -1,7 +1,16 @@
 package com.jarone.litterary.handlers;
 
+import android.widget.Toast;
+
+import com.jarone.litterary.LitterApplication;
+
 /**
  * Created by Adam on 2015-11-03.
  */
 public class ErrorHandler {
+
+    public static void d (String message) {
+        Toast toast = Toast.makeText(LitterApplication.getContext(), message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
